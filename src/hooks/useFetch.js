9922f172807151da418e5bdcf92541072a068f7a
arkadiusz-kleczek <arek.ak.wow@gmail.com>
2022-04-05 10:40:33 +0200

    Cleanup function and aborting fetch Requests

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,22 +1,45 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react"
 
 export const useFetch = (url) => {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState(null)
     const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
+        const controller = new AbortController()
+
         const fetchData = async () => {
             setIsPending(true)
-            const res = await fetch(url)
-            const json = await res.json()
-            setIsPending(false)
-            setData(json)
-        }
+
+            try {
+                const res = await fetch(url, {signal:controller.signal })
+                if(!res.ok) {
+                    throw new Error(res.statusText)
+                }
+                const data = await res.json()
+
+                setIsPending(false)
+                setData(data)
+                setError(null)
+            } catch (err) {
+                if (err.name === "AbortError") {
+                    console.log('the fetch was aborted')
+                } else {
+                    setIsPending(false)
+                    setError('Could not fetch the data')
+                    console.log(err.message)
+                }
+
+            }
+
+        } // end of fetchData function
 
         fetchData()
+        return () => {
+            controller.abort()
+        }
+
     }, [url])
 
-    return { data, isPending }
-
+    return { data, isPending, error }
 }
-
